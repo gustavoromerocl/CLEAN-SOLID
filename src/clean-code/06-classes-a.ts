c7c03b000de726cdb8457f6bf0182bd9bc234442
romerodevelopments <gandresrp@gmail.com>
2023-03-12 15:44:46 -0300

    Clean Code - Clases y comentarios: Herencia problematica

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,21 +1,57 @@
 (() => {
+  //No aplicando el principio de responsabilidad única
   type Gender = 'M' | 'F';
+
   class Person {
-    // public name: string;
-    // public gender: Gender;
-    // public birthdate: Date;
+    constructor(
+      public name       : string,
+      public gender     : Gender,
+      public birthdate  : Date
+    ) { }
+  }
+
+  class User extends Person {
+    public lastAccess: Date;
 
     constructor(
-      public name: string,
-      public gender: Gender,
-      public birthdate: Date
+      public email    : string,
+      public role     : string,
+      name            : string,
+      gender:         Gender,
+      birthdate:      Date
     ) {
-      //   this.name = name;
-      //   this.gender = gender;
-      //   this.birthdate = birthdate;
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
     }
   }
 
-  const newPerson = new Person('Fernando', 'M', new Date('1990-11-27'));
-  console.log({ newPerson })
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory : string,
+      public lastOpenFolder   : string,
+      email                   : string,
+      role                    : string,
+      name                    : string,
+      gender:                 Gender,
+      birthdate:              Date
+    ) { 
+      super(email,role, name, gender, birthdate );
+    }
+  }
+
+  const userSettings = new UserSettings(
+    'users/home',
+    '/home',
+    'gandresrp@gmail.com',
+    'admin',
+    'Gustavo',
+    'M',
+    new Date('1990-11-27'),
+  )
+  
+  console.log({ userSettings, areCredentialsValid: userSettings.checkCredentials() })
 })()
