1c874aa7cee453dd857af15f88b06682ad01a713
romerodevelopments <gandresrp@gmail.com>
2023-04-09 23:07:23 -0400

    Principios SOLID: Ejercicio segregacion de interfaz

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,35 @@
+interface Bird {
+  fly(): void;
+  eat(): void;
+  run(): void;
+  swim(): void;
+}
+
+class Tucan implements Bird {
+  public fly() {}
+  public eat() {}
+  public run() {}
+  public swim() {}
+}
+
+class Humminbird implements Bird {
+  public fly() {}
+  public eat() {}
+  public run() {}
+  public swim() {}
+}
+
+class Ostrich implements Bird {
+  public fly() {}
+  public eat() {}
+  public run() {}
+  public swim() {}
+}
+
+class Penguin implements Bird {
+  public fly() {}
+  public eat() {}
+  public run() {}
+  public swim() {}
+}
+
